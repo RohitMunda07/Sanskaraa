a4300857a5d1fd2f9d783218bb83d80c2179612c
rishabkumar03 <rishabkumar3.2005@gmail.com>
2025-08-12 01:56:43 +0530

    feat: add cookie-parser and cors dependencies; update database connection handling

--- Backend/src/db/index.js
+++ Backend/src/db/index.js
@@ -12,3 +12,14 @@ const connectDB = async () => {
 }
 
 export default connectDB
+.then(() => {
+    app.on((err) => {
+        console.log("Error while connecting to database", err);
+    })
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port: ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGODB Connection Failed !!", err);
+})
